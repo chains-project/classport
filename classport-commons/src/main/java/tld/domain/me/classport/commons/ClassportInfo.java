f630ddbfdb36b86b0f6497cdf87acd11dffd0197
Daniel Williams <dwilli@kth.se>
2024-04-23 19:50:25 +0200

    Add parent information to dependency metadata

--- classport-commons/src/main/java/tld/domain/me/classport/commons/ClassportInfo.java
+++ classport-commons/src/main/java/tld/domain/me/classport/commons/ClassportInfo.java
@@ -4,9 +4,13 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ClassportInfo {
+    String id();
+
     String artefact();
 
     String group();
 
     String version();
+
+    String parentId();
 }
--- maven-plugin/src/main/java/tld/domain/me/classport/plugin/EmbeddingMojo.java
+++ maven-plugin/src/main/java/tld/domain/me/classport/plugin/EmbeddingMojo.java
@@ -18,6 +18,8 @@ import org.apache.maven.artifact.Artifact;
  * limitations under the License.
  */
 
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -25,19 +27,23 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.shared.dependency.graph.DependencyCollectorBuilder;
+import org.apache.maven.shared.dependency.graph.DependencyCollectorBuilderException;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.DefaultProjectBuildingRequest;
+import org.apache.maven.project.ProjectBuildingRequest;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-// TODO:
-//  - The LifecyclePhase might need changing (?)
-//      See https://maven.apache.org/guides/introduction/introduction-to-the-lifecycle.html
-//  - Change the ResolutionScope?
-//      See https://maven.apache.org/plugin-tools/apidocs/org/apache/maven/plugins/annotations/ResolutionScope.html
-@Mojo(name = "classport", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE)
+// We don't mind too much about the default lifecycle, but the ResolutionScope has to be TEST
+// so that all dependencies are resolved
+@Mojo(name = "classport", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyCollection = ResolutionScope.TEST, requiresDependencyResolution = ResolutionScope.TEST)
 public class EmbeddingMojo
         extends AbstractMojo {
     /**
@@ -46,41 +52,83 @@ public class EmbeddingMojo
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
+    @Component
+    private MavenSession session;
+
+    /**
+     * The dependency collector builder to use.
+     */
+    @Component(hint = "default")
+    private DependencyCollectorBuilder dependencyCollectorBuilder;
+
+    private DependencyNode rootNode;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        Set<Artifact> afs = project.getArtifacts();
-        var log = getLog();
-        log.info("Found " + afs.size() + " artefacts.");
+        // For some reason unbeknownst to me, `Artifact`s retrieved from
+        // `DependencyNode::getArtifact` don't seem to contain the file path, regardless
+        // of what we set our dependency resolution/collection scopes to.
+        //
+        // There might be something I'm missing, but for now, we can just store the
+        // "full" artifacts in a map. Whenever we need a dependency's artifact, we just
+        // retrieve it from there (the ID should be the same so this is used as key)
+        Set<Artifact> completeArtifactSet = project.getArtifacts();
+        Map<String, Artifact> completeArtifactMap = completeArtifactSet.stream()
+                .collect(Collectors.toMap(Artifact::getId, a -> a));
+        getLog().info("Found " + completeArtifactMap.size() + " artefacts.");
 
         File localrepoRoot = new File("classport-files");
         // There's no way to differentiate between "the directory already exists"
         // and "failed to create the directory" without checking for existance too
         // so just allow overwriting as that's probably what we want anyway (?)
         localrepoRoot.mkdir();
-        for (Artifact a : afs) {
+
+        try {
+            ProjectBuildingRequest buildingRequest = new DefaultProjectBuildingRequest(
+                    session.getProjectBuildingRequest());
+
+            buildingRequest.setProject(project);
+            rootNode = dependencyCollectorBuilder.collectDependencyGraph(buildingRequest, null);
+
+            embedMetadata(rootNode, localrepoRoot, completeArtifactMap);
+
+            getLog().info("Embedding complete");
+        } catch (DependencyCollectorBuilderException exception) {
+            throw new MojoExecutionException("Cannot build project dependency graph", exception);
+        }
+    }
+
+    // TODO: Refactor to embed first, recursively call later
+    private void embedMetadata(DependencyNode node, File localrepoRoot, Map<String, Artifact> afs) {
+        node.getChildren().forEach(c -> {
+            Artifact a = afs.get(c.getArtifact().getId());
+            getLog().info("Processing " + a.getArtifactId());
             try {
                 HashMap<String, String> metadataPairs = new HashMap<>();
                 // TODO More SBOM stuff here
                 // - URL
-                // - Parent info
                 // - Checksums
                 // Also TODO: Can we programmatically populate this based on the
                 // annotation interface?
+                metadataPairs.put("id", a.getId());
                 metadataPairs.put("group", a.getGroupId());
                 metadataPairs.put("artefact", a.getArtifactId());
                 metadataPairs.put("version", a.getVersion());
+                metadataPairs.put("parentId", c.getParent() == null ? null : c.getParent().getArtifact().getId());
 
                 String artefactPath = getArtefactPath(a);
                 JarHelper pkgr = new JarHelper(a.getFile(),
                         new File(localrepoRoot + "/" + artefactPath),
-                        /* overwrite target if exists */ true);
-                log.info("Modifying " + a.getArtifactId());
+                        /* overwrite target if exists? */ true);
+                getLog().info("Embedding metadata for " + a.getArtifactId());
                 pkgr.embed(metadataPairs);
-            } catch (IOException e) {
-                log.error(e);
-            }
-        }
 
-        log.info("Embedding complete");
+                // Recurse on current node to get their children
+                embedMetadata(c, localrepoRoot, afs);
+            } catch (IOException e) {
+                getLog().error(e);
+            }
+
+        });
     }
 
     /**
