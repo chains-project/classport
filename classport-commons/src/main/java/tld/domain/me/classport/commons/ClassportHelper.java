653388fb74fc73dbd19a46c3c958172a21291189
Daniel Williams <dwilli@kth.se>
2024-05-07 00:55:08 +0200

    Add helper method to obtain an annotation object instance

---
+++ classport-commons/src/main/java/tld/domain/me/classport/commons/ClassportHelper.java
@@ -0,0 +1,39 @@
+package tld.domain.me.classport.commons;
+
+import java.lang.annotation.*;
+
+public class ClassportHelper {
+    public ClassportInfo getInstance(String id, String artefact, String group, String version, String[] childIds) {
+        return new ClassportInfo() {
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return ClassportInfo.class;
+            }
+
+            @Override
+            public String id() {
+                return id;
+            }
+
+            @Override
+            public String artefact() {
+                return artefact;
+            }
+
+            @Override
+            public String group() {
+                return group;
+            }
+
+            @Override
+            public String version() {
+                return version;
+            }
+
+            @Override
+            public String[] childIds() {
+                return childIds;
+            }
+        };
+    }
+}
