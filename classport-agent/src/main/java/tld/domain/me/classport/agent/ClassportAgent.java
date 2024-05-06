d713db60eefe83f2468087d5113096384b4d49a7
Daniel Williams <dwilli@kth.se>
2024-05-07 01:30:02 +0200

    Figure out circularities in agent class loading

--- classport-agent/src/main/java/tld/domain/me/classport/agent/ClassportAgent.java
+++ classport-agent/src/main/java/tld/domain/me/classport/agent/ClassportAgent.java
@@ -13,6 +13,17 @@ public class ClassportAgent {
     private static final HashMap<String, ClassportInfo> sbom = new HashMap<>();
 
     public static void premain(String argument, Instrumentation instrumentation) {
+        // Force classes to load early.
+        //
+        // If we try doing this after adding the transformer, it will try to transform
+        // PrintStream, which doesn't contain an annotation, so we print the class,
+        // which requires PrintStream [...]
+        System.out.println("[Agent] Adding shutdown hook...");
+        Thread printingHook = new Thread(() -> {
+            System.out.println("All loaded dependency classes: " + sbom);
+        });
+        Runtime.getRuntime().addShutdownHook(printingHook);
+
         instrumentation.addTransformer(new ClassFileTransformer() {
             @Override
             public byte[] transform(Module module,
@@ -27,7 +38,8 @@ public class ClassportAgent {
                         sbom.put(name, ann);
                         System.out.println("Loading class " + name + " (from " + ann.group() + ":" + ann.artefact()
                                 + ", version " + ann.version() + ")");
-                    }
+                    } else
+                        System.out.println("Loaded class without annotation: " + name);
                 } else {
                     System.out.println("[Agent] Re(loaded|defined) class '" +
                             typeIfLoaded.getName() + "'");
@@ -37,12 +49,5 @@ public class ClassportAgent {
                 return null;
             }
         });
-
-        System.out.println("Adding shutdown hook...");
-
-        Thread printingHook = new Thread(() -> {
-            System.out.println("All loaded dependency classes: " + sbom);
-        });
-        Runtime.getRuntime().addShutdownHook(printingHook);
     }
 }
