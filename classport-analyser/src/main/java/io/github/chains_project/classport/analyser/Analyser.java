5907e3da36625aeb86c70ccbd5210208ecd04301
Daniel Williams <dwilli@kth.se>
2024-05-25 19:21:39 +0200

    Refactor in preparation for adding codegen method

--- classport-analyser/src/main/java/io/github/chains_project/classport/analyser/Analyser.java
+++ classport-analyser/src/main/java/io/github/chains_project/classport/analyser/Analyser.java
@@ -21,15 +21,14 @@ public class Analyser {
     // TODO: Refactor the Maven plugin's JarHelper, put in into classport-commons,
     // and use this from there.
     private static final byte[] magicBytes = new byte[] { (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE };
-    private static final HashMap<String, ClassportInfo> sbom = new HashMap<>();
 
-    public static void main(String[] args) {
-        if (args.length != 1 || !args[0].endsWith(".jar")) {
-            System.err.println("Expected JAR file as argument");
-            System.exit(1);
-        }
-
-        try (JarFile jar = new JarFile(args[0])) {
+    private static HashMap<String, ClassportInfo> getSBOM(JarFile jar) {
+        HashMap<String, ClassportInfo> sbom = new HashMap<>();
+        try {
+            // TODO: There is some code duplication here. This same thing is used in the
+            // io.github.chains_project.classport.maven_plugin.JarHelper class.
+            // It would be a lot better to provide an iterator in the commons package that
+            // only yields class files to skip the boilerplate/repetition, or move this fn
             Enumeration<JarEntry> entries = jar.entries();
             while (entries.hasMoreElements()) {
                 JarEntry entry = entries.nextElement();
@@ -48,11 +47,38 @@ public class Analyser {
                 }
             }
         } catch (IOException e) {
-            System.err.println("Unable to process JAR: " + e.getMessage());
+            System.err.println("Error while printing tree: " + e.getMessage());
         }
 
+        return sbom;
+    }
+
+    private static void printDepTree(JarFile jar) {
+        HashMap<String, ClassportInfo> sbom = getSBOM(jar);
         ClassportProject proj = new ClassportProject(sbom);
         System.out.println("Dependency tree:");
         proj.writeTree(new PrintWriter(System.out));
+    }
+
+    public static void main(String[] args) {
+        // TODO: Use picocli for a nicer interface
+        if (args.length != 2 || (!args[0].equals("-generateTestJar") && !args[0].equals("-printTree"))) {
+            System.err.println("Usage: -<printTree|generateTestJar> <jarFile>");
+            System.exit(1);
+        }
+
+        JarFile jar;
+        try {
+            jar = new JarFile(args[1]);
+        } catch (IOException e) {
+            // Re-throw since javac doesn't know that System.exit() won't return
+            throw new RuntimeException("Failed to parse " + args[1] + " as JAR file: " + e.getMessage());
+        }
+
+        if (args[0].equals("-printTree")) {
+            printDepTree(jar);
+        } else if (args[0].equals("-generateTestJar")) {
+            // unimplemented
+        }
     }
 }
