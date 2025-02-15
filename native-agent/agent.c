e252cad44d5b715e6a483f377d8232a809d58755
Serena Cofano <cofanoserena@gmail.com>
2025-02-15 12:10:03 +0100

    refactor and fix problem in using getAnnotations()

--- native-agent/agent.c
+++ native-agent/agent.c
@@ -4,95 +4,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-#define CONSTANT_UTF8 1
-#define CONSTANT_INTEGER 3
-#define CONSTANT_FLOAT 4
-#define CONSTANT_LONG 5
-#define CONSTANT_DOUBLE 6
-#define CONSTANT_CLASS 7
-#define CONSTANT_STRING 8
-#define CONSTANT_FIELDREF 9
-#define CONSTANT_METHODREF 10
-#define CONSTANT_INTERFACEMETHODREF 11
-#define CONSTANT_NAME_AND_TYPE 12
-#define CONSTANT_METHODHANDLE 15
-#define CONSTANT_METHOD_TYPE 16
-#define CONSTANT_INVOKE_DYNAMIC 18
-#define CONSTANT_MODULE 19
-#define CONSTANT_PACKAGE 20
-
-void parse_constant_pool(unsigned char *constant_pool, jint constant_pool_count, char *class_signature, char *method_name) {
-    int found_annotation = 0;
-    char *value = NULL;
-    for (int i = 0; i < constant_pool_count; i++) {
-        unsigned char tag = constant_pool[i];
-
-        switch (tag) {
-            case CONSTANT_UTF8: {
-                uint16_t length;
-                memcpy(&length, &constant_pool[i + 1], sizeof(uint16_t));
-                length = ntohs(length);
-                char *utf8 = (char *)malloc(length + 1);
-                memcpy(utf8, &constant_pool[i + 3], length);
-                utf8[length] = '\0';
-                
-                if (strcmp(utf8, "Lio/github/chains_project/classport/commons/ClassportInfo;") == 0) {
-                    printf("--------------------\n");
-                    printf("Found custom annotation: %s\n", utf8);
-                    printf("Class: %s\n", class_signature);
-                    printf("Method: %s\n", method_name);
-                    found_annotation = 1;
-                } else if (strcmp(utf8, "RuntimeVisibleAnnotations") == 0) {
-                    found_annotation = 0;
-                } else if (found_annotation && strcmp(utf8, "isDirectDependency") != 0) {
-                    if (value) {
-                        printf("%s: %s\n", value, utf8);
-                        free(value);
-                        value = NULL;
-                    } else {
-                        value = strdup(utf8);
-                    }
-                }
-                
-                free(utf8);
-                i += 2 + length;
-                break;
-            }
-            case CONSTANT_INTEGER:
-                i += 4;
-                break;
-            case CONSTANT_FLOAT:
-                i += 4;
-                break;
-            case CONSTANT_LONG:
-                i += 8;
-                break;
-            case CONSTANT_DOUBLE:
-                i += 8;
-                break;
-            case CONSTANT_CLASS:
-            case CONSTANT_STRING:
-            case CONSTANT_METHOD_TYPE:
-            case CONSTANT_MODULE:
-            case CONSTANT_PACKAGE:
-                i += 2;
-                break;
-            case CONSTANT_FIELDREF:
-            case CONSTANT_METHODREF:
-            case CONSTANT_INTERFACEMETHODREF:
-            case CONSTANT_NAME_AND_TYPE:
-            case CONSTANT_INVOKE_DYNAMIC:
-                i += 4;
-                break;
-            case CONSTANT_METHODHANDLE:
-                i += 3;
-                break;
-            default:
-                fprintf(stderr, "Unknown constant pool type '%d'\n", tag);
-                break;
-        }
-    }
-}
+static char jar_path[1024];
 
 // Callback for Method Entry
 void JNICALL onMethodEntry(jvmtiEnv *jvmti_env, JNIEnv *jni_env, jthread thread, jmethodID method) {
@@ -101,9 +13,6 @@ void JNICALL onMethodEntry(jvmtiEnv *jvmti_env, JNIEnv *jni_env, jthread thread,
     char *class_signature = NULL;
     jclass declaring_class;
     jvmtiError err;
-    jint constant_pool_count;
-    jint constant_pool_byte_count;
-    unsigned char *constant_pool;
 
     // Get the method's declaring class
     err = (*jvmti_env)->GetMethodDeclaringClass(jvmti_env, method, &declaring_class);
@@ -118,7 +27,7 @@ void JNICALL onMethodEntry(jvmtiEnv *jvmti_env, JNIEnv *jni_env, jthread thread,
     } 
     // Check if the method name is "customSleepingThread"
     if (err == JVMTI_ERROR_NONE && strcmp(method_name, "customSleepingThread") == 0) {
-        printf("Executing customSleepingThread method\n");
+        // printf("Executing customSleepingThread method\n");
         jclass class_class = (*jni_env)->FindClass(jni_env, "java/lang/Class");
         if (class_class == NULL) {
             fprintf(stderr, "Error: Unable to find class java/lang/Class in onMethodEntry\n");
@@ -163,71 +72,16 @@ void JNICALL onMethodEntry(jvmtiEnv *jvmti_env, JNIEnv *jni_env, jthread thread,
     //     printf("Executing Class: %s, Method: %s%s\n", class_signature, method_name, method_signature);
     // }
 
-    // err = (*jvmti_env)->GetConstantPool(jvmti_env, declaring_class, &constant_pool_count, &constant_pool_byte_count, &constant_pool);
-    // if (err != JVMTI_ERROR_NONE) {
-    //     fprintf(stderr, "Failed to get constant pool: %d\n", err);
-    // }
-
-    // Parse the Constant Pool
-    // parse_constant_pool(constant_pool, constant_pool_byte_count, class_signature, method_name);
-
-
-
     // Deallocate JVMTI memory
     if (class_signature) (*jvmti_env)->Deallocate(jvmti_env, (unsigned char *)class_signature);
     if (method_name) (*jvmti_env)->Deallocate(jvmti_env, (unsigned char *)method_name);
     if (method_signature) (*jvmti_env)->Deallocate(jvmti_env, (unsigned char *)method_signature);
-    // if (constant_pool) (*jvmti_env)->Deallocate(jvmti_env, constant_pool);
 }
 
-//Callback method
 void onVMInit(jvmtiEnv *jvmti_env, JNIEnv* jni_env, jthread thread)
 {
     printf("loading jar...\n");
-    const char* javaNetUrlClassName = "java/net/URL";
-    char* jarPath = (char*) "file:///Users/serena/Dottorato/KTH/classport-dev/classport/classport-commons/target/classport-commons-0.1.0-SNAPSHOT.jar";
-    jclass urlClass = (*jni_env)->FindClass(jni_env, javaNetUrlClassName);
-    if(urlClass == NULL){
-        char msg[50];
-        sprintf(msg, "couldn't find class: %s\n", javaNetUrlClassName);
-        (*jni_env)->FatalError(jni_env,(char *) msg);
-    }
-    printf("urlClass created\n");
-
-    jmethodID urlConstructor = (*jni_env)->GetMethodID(jni_env,urlClass, "<init>", "(Ljava/lang/String;)V");
-    jstring jarPathJString = (*jni_env)->NewStringUTF(jni_env,jarPath);
-    jobject url = (*jni_env)->NewObject(jni_env,urlClass, urlConstructor, jarPathJString);
-    (*jni_env)->ReleaseStringUTFChars(jni_env,jarPathJString, NULL);
-    if(url == NULL){
-        (*jni_env)->FatalError(jni_env,"Couldn't create URL object");
-    }
-    printf("jar url created\n");
-
-    jobjectArray urls = (*jni_env)->NewObjectArray(jni_env,1, (*jni_env)->FindClass(jni_env,javaNetUrlClassName), NULL);
-    (*jni_env)->SetObjectArrayElement(jni_env,urls, 0, url);
-
-    jclass classLoaderClass = (*jni_env)->FindClass(jni_env,"java/net/URLClassLoader");
-    jmethodID constructor = (*jni_env)->GetMethodID(jni_env,classLoaderClass, "<init>", "([Ljava/net/URL;)V");
-    jobject classLoader = (*jni_env)->NewObject(jni_env,classLoaderClass, constructor, urls);
-    printf("classloader created \n");
-
-    jclass clazz = (*jni_env)->FindClass(jni_env,"java/lang/Class");
-    jmethodID classForNameMethod = (*jni_env)->GetStaticMethodID(jni_env,clazz, "forName", "(Ljava/lang/String;ZLjava/lang/ClassLoader;)Ljava/lang/Class;");
-    if(classForNameMethod == NULL){
-        (*jni_env)->FatalError(jni_env,"Class.forName(String,boolean,java.lang.ClassLoader)java.lang.Class method not found\n");
-    }
-
-    char *jarInitClass = "io.github.chains_project.classport.commons.ClassportInfo";
-    jstring classToLoad = (*jni_env)->NewStringUTF(jni_env,jarInitClass);
-    //bool t = true;
-    jclass jarClass = (jclass) (*jni_env)->CallStaticObjectMethod(jni_env,clazz, classForNameMethod, classToLoad, 0, classLoader);
-    (*jni_env)->ReleaseStringUTFChars(jni_env,classToLoad, NULL);
-    if(jarClass == NULL){
-        printf("jarClass found %s\n", jarClass);
-        char msg[100];
-        sprintf(msg, "jar init class not found: %s\n", jarInitClass);
-        // (*jni_env)->FatalError(jni_env,(char *) msg);
-    }
+    (*jvmti_env)->AddToBootstrapClassLoaderSearch(jvmti_env, jar_path);
 }
 
 // Used for attaching at start-up time using the -agentpath option
@@ -235,28 +89,60 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm, char *options, void *reserved) 
     jvmtiEnv *jvmti;
     jvmtiCapabilities      capabilities = {0};
     jvmtiError             error;
-    jvmtiEventCallbacks    callbacks;
+    jvmtiEventCallbacks    callbacks = {0};
 
     // Get the JVMTI environment
     jint res = (*jvm)->GetEnv(jvm, (void **)&jvmti, JVMTI_VERSION_1_2);
-    if (res != JNI_OK) {
+    if (res != JNI_OK || jvmti == NULL) {
         printf("ERROR: Unable to access JVMTI\n");
         return JNI_ERR;
     }
 
+    // Retrieve potential capabilities
     error = (*jvmti)->GetPotentialCapabilities(jvmti, &capabilities);
     if (error == JVMTI_ERROR_NONE) {
-        (*jvmti)->AddCapabilities(jvmti, &capabilities);
+        error = (*jvmti)->AddCapabilities(jvmti, &capabilities);
+        if (error != JVMTI_ERROR_NONE) {
+            printf("ERROR: AddCapabilities failed: %d\n", error);
+            return JNI_ERR;
+        }
+    } else {
+        printf("ERROR: GetPotentialCapabilities failed: %d\n", error);
+        return JNI_ERR;
     }
 
-    (void)memset(&callbacks, 0, sizeof(callbacks));
     // Register the callback and enable the Method Entry event
     callbacks.MethodEntry = &onMethodEntry;
-    callbacks.VMInit = (void*)&onVMInit;
+    callbacks.VMInit = onVMInit;
 
-    (*jvmti)->SetEventCallbacks(jvmti, &callbacks, sizeof(callbacks));
-    (*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
-    (*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT , NULL);
+    error = (*jvmti)->SetEventCallbacks(jvmti, &callbacks, sizeof(callbacks));
+    if (error != JVMTI_ERROR_NONE) {
+        printf("ERROR: SetEventCallbacks failed: %d\n", error);
+        return JNI_ERR;
+    }
+
+    // Enable JVMTI events
+    error = (*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
+    if (error != JVMTI_ERROR_NONE) {
+        printf("ERROR: Enabling METHOD_ENTRY event failed: %d\n", error);
+        return JNI_ERR;
+    }
+
+    error = (*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, NULL);
+    if (error != JVMTI_ERROR_NONE) {
+        printf("ERROR: Enabling VM_INIT event failed: %d\n", error);
+        return JNI_ERR;
+    }
+
+    // Set the jar path
+    if (options != NULL) {
+        strncpy(jar_path, options, sizeof(jar_path) - 1);
+        jar_path[sizeof(jar_path) - 1] = '\0'; // Ensure null-terminated
+        printf("Agent options: %s\n", jar_path);
+    } else {
+        printf("The full path to classport-commons/target/classport-commons-0.1.0-SNAPSHOT.jar is needed. See doc.\n");
+        return JNI_ERR;
+    }
 
     printf("Agent_OnLoad\n");
 
