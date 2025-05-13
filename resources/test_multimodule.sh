503b21eb5edb919bd4e0f52f00b108dd9a3cb504
Serena Cofano <cofanoserena@gmail.com>
2025-05-13 09:45:43 +0200

    script for running multimodule application

---
+++ resources/test_multimodule.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# clean classport-files
+./clean.sh --classport-files
+
+# clean multi mvn project
+cd multi
+mvn clean
+
+# run embedding plugin to embed annotations into multi project
+mvn io.github.chains-project:classport-maven-plugin:0.1.0-SNAPSHOT:embed
+
+# merge the classport-files
+cd ..
+./post_process_local_repo.sh multi
+
+# move to submodule and first package them with the new annotated maven repository
+cd multi/submulti
+mvn package -Dmaven.repo.local=../all-classport-files
+
+# move to the root and package everything
+cd ..
+mvn package -Dmaven.repo.local=all-classport-files
+
+# run the retriever
+cd ..
+./run_retriever_multi.sh
