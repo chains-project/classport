de47256646771af2e9ac5fdd001b01244c560af9
Aman Sharma <mannu.poski10@gmail.com>
2025-06-27 17:17:37 +0200

    Implement initializeCSVHeader separately

--- classport-instr-agent/src/main/java/io/github/chains_project/classport/instrumentation/granularity/ClassInvocation.java
+++ classport-instr-agent/src/main/java/io/github/chains_project/classport/instrumentation/granularity/ClassInvocation.java
@@ -1,5 +1,8 @@
 package io.github.chains_project.classport.instrumentation.granularity;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +16,20 @@ public class ClassInvocation implements RecordingStrategy {
 
 	public ClassInvocation(Path outputPath) {
 		this.outputPath = outputPath;
+	}
+
+	@Override
+	public void initializeCSVHeader(Path outputPath) {
+		// Write header to the file if it doesn't exist or is empty
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath.toFile(), true))) {
+			File file = outputPath.toFile();
+			if (file.length() == 0) {
+				writer.write("group,artefact,version\n");
+				writer.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
--- classport-instr-agent/src/main/java/io/github/chains_project/classport/instrumentation/granularity/RecordingStrategy.java
+++ classport-instr-agent/src/main/java/io/github/chains_project/classport/instrumentation/granularity/RecordingStrategy.java
@@ -7,18 +7,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public interface RecordingStrategy {
-	default void initializeCSVHeader(Path outputPath) {
-		// Write header to the file if it doesn't exist or is empty
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath.toFile(), true))) {
-			File file = outputPath.toFile();
-			if (file.length() == 0) {
-				writer.write("Class,Method,sourceProjectId,isDirect,id,artefact,group,version,childIds\n");
-				writer.flush();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	void initializeCSVHeader(Path outputPath);
 	void addToInvokeLater(String content);
 	void initializeBackgroundWriter();
 	void writeToFile();
