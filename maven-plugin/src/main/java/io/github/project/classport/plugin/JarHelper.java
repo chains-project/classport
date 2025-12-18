44271485754c7e3e5388d840722153aee6cd52fa
Aman Sharma <mannu.poski10@gmail.com>
2025-12-18 17:50:10 +0100

    perf: parallelize streaming of jar entries

--- maven-plugin/src/main/java/io/github/project/classport/plugin/JarHelper.java
+++ maven-plugin/src/main/java/io/github/project/classport/plugin/JarHelper.java
@@ -1,39 +1,33 @@
 package io.github.project.classport.plugin;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PushbackInputStream;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
 
 import io.github.project.classport.commons.AnnotationConstantPool;
 import io.github.project.classport.commons.ClassportInfo;
+import io.github.project.classport.commons.Utility;
 
 class JarHelper {
     private final File source;
     private final File target;
     private final boolean overwrite;
 
-    // All class files begin with the magic bytes 0xCAFEBABE
-    // TODO: Refactor this class and put it in classport-commons
-    // TODO: Expose this constant
-    private static final byte[] magicBytes = new byte[] { (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE };
 
     public JarHelper(File source, File target) {
         this(source, target, false);
@@ -56,6 +50,10 @@ class JarHelper {
     // Slightly revised version of `buildUnsignedManifest` from Maven's JarSigner:
     // https://github.com/apache/maven-jarsigner/blob/9bc044710ff5c13bc540992a9a998453f213fd1b/src/main/java/org/apache/maven/shared/jarsigner/JarSignerUtil.java
     private Manifest getUnsignedManifest(Manifest m) {
+        if (m == null) {
+            return new Manifest();
+        }
+        
         Manifest unsigned = new Manifest(m);
         unsigned.getEntries().clear();
 
@@ -80,11 +78,6 @@ class JarHelper {
     }
     
     public void embed(ClassportInfo metadata) throws IOException {
-        AnnotationConstantPool acp = new AnnotationConstantPool(metadata);
-        AnnotationConstantPool.ConstantPoolData cpData = acp.getNewEntries();
-        File tmpdir = Files.createTempDirectory("classport").toFile();
-        extractTo(tmpdir);
-        
         if (target.exists() && !overwrite)
             throw new IOException("File or directory " + target + " already exists. Skipping embed...");
         if (source.isDirectory())
@@ -92,59 +85,78 @@ class JarHelper {
         
         target.getParentFile().mkdirs();
         
-        try (JarOutputStream out = new JarOutputStream(
-            new BufferedOutputStream(new FileOutputStream(target)))) {
-                Files.walkFileTree(tmpdir.toPath(), new SimpleFileVisitor<Path>() {
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        String relPath = tmpdir.toPath().relativize(file).toString();
-                        
-                        // Signed JARs won't work since we edit the contents, so remove signatures
-                        if (isSignatureFile(relPath))
-                        return FileVisitResult.CONTINUE;
-
-                    out.putNextEntry(new JarEntry(relPath));
-
-                    // Remove signature attributes from manifest file
-                    if (relPath.equals("META-INF/MANIFEST.MF")) {
-                        Manifest manifest = new Manifest(new FileInputStream(file.toFile()));
-                        Manifest unsigned = getUnsignedManifest(manifest);
-                        unsigned.write(out);
-                    } else {
-                        // We need to "peek" at the first 4 bytes to see if it's a classfile or not
-                        try (PushbackInputStream in = new PushbackInputStream(new FileInputStream(file.toFile()), 4)) {
-                            byte[] firstBytes = in.readNBytes(4);
-                            in.unread(firstBytes);
-
-                            if (Arrays.equals(firstBytes, magicBytes)) {
-                                // It's a classfile, embed the metadata
-                                byte[] classBytes = in.readAllBytes();
-                                byte[] modifiedCpBytes = acp.injectAnnotation(classBytes, cpData);
-
-                                out.write(modifiedCpBytes);
-                            } else {
-                                // Not a classfile, just stream the entire contents directly
-                                in.transferTo(out);
-                            }
-                        }
-                    }
-
-                    out.closeEntry();
-                    return FileVisitResult.CONTINUE;
+        AnnotationConstantPool acp = new AnnotationConstantPool(metadata);
+        AnnotationConstantPool.ConstantPoolData cpData = acp.getNewEntries();
+        
+        Path inJar = source.toPath();
+        Path outJar = target.toPath();
+        
+        try (JarInputStream jis = new JarInputStream(Files.newInputStream(inJar));
+             JarOutputStream jos = new JarOutputStream(
+                 new BufferedOutputStream(Files.newOutputStream(outJar)), 
+                 getUnsignedManifest(jis.getManifest()))) {
+            
+            JarEntry entry;
+            byte[] buffer = new byte[8192];
+            
+            while ((entry = jis.getNextJarEntry()) != null) {
+                // Skip signature files
+                if (isSignatureFile(entry.getName())) {
+                    continue;
                 }
-
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    String relPath = tmpdir.toPath().relativize(dir).toString() + "/";
-
-                    // .jar files don't include the root ("/")
-                    if (dir.equals(tmpdir.toPath()))
-                        return FileVisitResult.CONTINUE;
-
-                    out.putNextEntry(new JarEntry(relPath));
-                    out.closeEntry();
-                    return FileVisitResult.CONTINUE;
+                
+                // Skip MANIFEST.MF since it's already written via JarOutputStream constructor
+                if (entry.getName().equals("META-INF/MANIFEST.MF")) {
+                    // Still need to read and discard the entry data
+                    readEntryBytes(jis, buffer);
+                    continue;
                 }
-            });
+                
+                // Copy metadata
+                JarEntry newEntry = new JarEntry(entry.getName());
+                newEntry.setTime(entry.getTime());
+                jos.putNextEntry(newEntry);
+                
+                // Read the entire entry into memory
+                byte[] entryBytes = readEntryBytes(jis, buffer);
+                
+                if (shouldReplace(entry, entryBytes)) {
+                    byte[] replacementBytes = replacementBytes(entryBytes, acp, cpData);
+                    jos.write(replacementBytes);
+                } else {
+                    // Copy entry as-is
+                    jos.write(entryBytes);
+                }
+                
+                jos.closeEntry();
+            }
         }
+    }
+    
+    private byte[] readEntryBytes(JarInputStream jis, byte[] buffer) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int read;
+        while ((read = jis.read(buffer)) != -1) {
+            baos.write(buffer, 0, read);
+        }
+        return baos.toByteArray();
+    }
+    
+    private boolean shouldReplace(JarEntry entry, byte[] entryBytes) {
+        // Check if it's a class file
+        if (!entry.getName().endsWith(".class") || entryBytes.length < 4) {
+            return false;
+        }
+        
+        // Check magic bytes
+        byte[] firstBytes = Arrays.copyOfRange(entryBytes, 0, 4);
+        return Arrays.equals(firstBytes, Utility.MAGIC_BYTES);
+    }
+    
+    private byte[] replacementBytes(byte[] classBytes, 
+                                     AnnotationConstantPool acp, 
+                                     AnnotationConstantPool.ConstantPoolData cpData) {
+        return acp.injectAnnotation(classBytes, cpData);
     }
 
     public void extract() throws IOException {
