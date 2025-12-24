377809ad8a932f05c7559ab5cbca99a421406e17
Aman Sharma <mannu.poski10@gmail.com>
2025-12-24 13:41:24 +0100

    Aman's approach
    stacked-commit: true

--- maven-plugin/src/main/java/io/github/project/classport/plugin/EmbeddingMojo.java
+++ maven-plugin/src/main/java/io/github/project/classport/plugin/EmbeddingMojo.java
@@ -29,6 +29,7 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
 
+import io.github.project.classport.commons.AnnotationConstantPool;
 import io.github.project.classport.commons.ClassportHelper;
 import io.github.project.classport.commons.ClassportInfo;
 import io.github.project.classport.commons.Utility;
@@ -95,6 +96,8 @@ public class EmbeddingMojo
 
     private void embedDirectory(Artifact a, File dirToWalk) throws IOException, MojoExecutionException {
         ClassportInfo metadata = getMetadata(a);
+        AnnotationConstantPool acp = new AnnotationConstantPool(metadata);
+        AnnotationConstantPool.ConstantPoolData cpData = acp.getNewEntries();
         if (!dirToWalk.exists()) {
             getLog().info("No classes compiled for " + project.getArtifactId() + ". Skipping.");
             return;
@@ -109,9 +112,9 @@ public class EmbeddingMojo
                     byte[] bytes = in.readAllBytes();
 
                     if (Arrays.equals(Arrays.copyOfRange(bytes, 0, 4), Utility.MAGIC_BYTES)) {
-                        MetadataAdder adder = new MetadataAdder(bytes);
+                        byte[] modifiedCpBytes = acp.injectAnnotation(bytes, cpData);
                         try (FileOutputStream out = new FileOutputStream(file.toFile())) {
-                            out.write(adder.add(metadata));
+                            out.write(modifiedCpBytes);
                         }
                     }
                 }
